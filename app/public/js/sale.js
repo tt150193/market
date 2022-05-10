2da35f60ef75a3cd229c813b648dc645807e099c
Vo Thanh Lam <vothanhlam1793@gmail.com>
2022-05-11 06:16:56 +0700

    add sidebar

--- app/public/js/sale.js
+++ app/public/js/sale.js
@@ -1,18 +1,68 @@
+Vue.component('item-sidebar', {
+    props: ['name', 'title', 'hash'],
+    template: `
+        <a :href="'#' + hash" :class="getItemColor()" @click="goPage()">{{title}}</a>
+    `,
+    data(){
+        return {
+
+        }
+    },
+    computed: {
+        page(){
+            return this.$store.state.hashpage;
+        }
+    },
+    methods: {
+        goPage(){
+            this.$store.dispatch('toPage', this.$props.name);
+        },
+        getItemColor(){
+            if(this.$props.name == this.page){
+                return "list-group-item list-group-item-action list-group-item-info"
+            } else {
+                return "list-group-item list-group-item-action"
+            }
+        }
+    },
+    created(){
+
+    }
+})
+if(location.hash.length == 0){
+    location.hash = "#common";
+}
 const store = new Vuex.Store({
     state: {
-        
+        customers: [],
+        leads: [],
+        hashpage: location.hash.substring(1,location.hash.length)
     },
     mutations: {
-        
+        updateCustomers(state, payload){
+            state.leads = payload.filter(function(customer){
+                return customer.type == "LEAD";
+            })
+            state.customers = payload.filter(function(customer){
+                return customer.type == "CUSTOMER";
+            })
+        },
+        toPage(state, payload){
+            state.hashpage = payload;
+        }
     },
     actions: {
+        toPage(context, payload){
+            context.commit('toPage', payload);
+        },
         getAllCustomersOfSale(context, payload) {
             graphql(`
                 query {
-                    allCustomers(where: {users_some: {id: "627a62e20501d70ee97375ba"}}){
+                    allCustomers(where: {users_some: {id: "`+ user.id +`"}}){
                         id
                         name
                         code
+                        type
                         notes {
                             title
                             content
@@ -20,16 +70,25 @@ const store = new Vuex.Store({
                     }
                 }
             `, {}).then(data=>{
-                console.log(data);
+                context.commit("updateCustomers", data.data.allCustomers);
             })
         }
     }
 });
 
 var app = new Vue({
+    el: "#app",
     store,
     data: {
 
+    },
+    computed: {
+        customers(){
+            return this.$store.state.customers;
+        },
+        leads(){
+            return this.$store.state.leads;
+        }
     },
     methods: {
 
